2c38231970bf174b114f3ac0229170aa7368f203
Emilly <163938818+mllyyyy@users.noreply.github.com>
2025-11-22 21:56:54 -0300

    Tela perfil idoso e cuidador atualizadas

--- app/idoso/perfil.js
+++ app/idoso/perfil.js
@@ -1,4 +1,5 @@
 import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { supabase } from '../../utils/supabase';
 
@@ -12,20 +13,53 @@ const signOut = async () => {
 };
 export default function Perfil() {
 
+    const router = useRouter();
+
     return (
         <SafeAreaView style={styles.safeArea}>
+            
+            <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+                <Ionicons name="arrow-back" size={24} color="#321904" />
+            </TouchableOpacity>
+
+
             <View style={styles.container}>
+
                 <View style={styles.topBox}>
-                    {/* Cabeçalho */}
                     <Text style={styles.greetingText}>Perfil</Text>
                     <Ionicons name="person-outline" size={80} color="#321904" />
+
+                    <Text style={styles.profileName}>Nome do idoso</Text>
                 </View>
-                {/* Botão Sair */}
+
+                <View style={styles.middleButtons}>
+                    <TouchableOpacity style={[styles.button, styles.primaryButton]}
+                        onPress={() => router.push('/idoso/dadosConta')}>
+                        <Text style={styles.buttonText}>Dados da minha conta</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={[styles.button, styles.primaryButton]}
+                        onPress={() => router.push('/idoso/contatosEmergencia')}>
+                        <Text style={styles.buttonText}>Contatos de emergência</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={[styles.button, styles.primaryButton]}
+                        onPress={() => router.push('/idoso/dadosSaude')}>
+                        <Text style={styles.buttonText}>Meus dados de saúde</Text>
+                    </TouchableOpacity>
+
+                     <TouchableOpacity style={[styles.button, styles.primaryButton]}
+                        onPress={() => router.push('/idoso/ajuda')}>
+                        <Text style={styles.buttonText}>Ajuda/Suporte</Text>
+                    </TouchableOpacity>
+                </View>
+
                 <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={[styles.button, styles.primaryButton]} onPress={signOut}>
+                    <TouchableOpacity style={[styles.button, styles.signOutButton]} onPress={signOut}>
                         <Text style={styles.buttonText}>Sair da Conta</Text>
                     </TouchableOpacity>
                 </View>
+
             </View>
         </SafeAreaView>
     );
@@ -36,44 +70,82 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
     },
+
+    backButton: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        borderWidth: 2,
+        borderColor: "#321904",
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+        marginLeft: 15,
+        zIndex: 10,
+        position: 'absolute',
+        top: 20,
+    },
+
     container: {
         flex: 1,
         justifyContent: 'space-between'
     },
 
     topBox: {
-        backgroundColor: '#F28B0C',
+        backgroundColor: '#fdba62ff',
         borderBottomLeftRadius: 25,
         borderBottomRightRadius: 25,
         paddingVertical: 50,
         paddingHorizontal: 20,
         alignItems: 'center',
+        paddingTop: 60,
     },
+
     greetingText: {
         fontSize: 25,
         color: '#321904',
         fontWeight: 'bold',
         marginBottom: 15,
+        marginTop: -20,
     },
-    profileIcon: {
-        marginTop: 5,
+
+    profileName: {
+        fontSize: 20,
+        color: '#321904',
+        marginTop: 10,
+        fontWeight: "600"
+    },
+
+    middleButtons: {
+        width: "100%",
+        alignItems: "center",
+        marginTop: 0,
+        gap: 20
     },
 
     buttonContainer: {
         width: '100%',
-        maxWidth: 300,
+        maxWidth: 370,
         alignSelf: 'center',
         marginBottom: 50,
     },
+
     button: {
         paddingVertical: 14,
         borderRadius: 8,
         alignItems: 'center',
-        width: '100%',
+        width: '85%',
+        alignSelf: "center"
     },
+
     primaryButton: {
         backgroundColor: '#F28B0C',
     },
+
+    signOutButton: {
+        backgroundColor: '#FBB65A',
+    },
+
     buttonText: {
         color: '#321904',
         fontWeight: 'bold',
